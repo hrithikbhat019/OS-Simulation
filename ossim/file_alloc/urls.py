91565f0f62dcb7dc5d49a61d5a7ad704d4c1484f
suraj44 <suraj1998@gmail.com>
2018-03-31 15:29:35 +0530

    added linked file allocation

--- ossim/file_alloc/urls.py
+++ ossim/file_alloc/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url,include
+from django.urls import path
 from django.contrib import admin
 from . import views
 
 app_name = 'file_alloc'
 urlpatterns =[
 
-    url(r'^$',views.home.as_view() ),
+    path('contiguous',views.contiguous.as_view() ),
+    path('linked',views.linked.as_view() ),
 
 ]
--- ossim/file_alloc/views.py
+++ ossim/file_alloc/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 
-class home(TemplateView):
-	template_name = "file_alloc.html"
+class contiguous(TemplateView):
+	template_name = "contiguous_file_alloc.html"
+class linked(TemplateView):
+	template_name = "linked_file_allocation.html"
 
