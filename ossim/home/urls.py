a2abe3799c469f63cce3dd64e17fb9a070933053
16CO142 <shreyaspandith1210@gmail.com>
2018-04-02 00:16:45 +0530

    desktop added

--- ossim/home/urls.py
+++ ossim/home/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.urls import path
+
 from . import views
 
 app_name = 'home'
 urlpatterns =[
 
-    url(r'^$',views.main )
+    url(r'^$',views.main ),
+    path('desktop/',views.Desktop ,name='desktop'),
+
+    path('desktop/Task.html/', views.Task,name='task'),
+
 
 ]
--- ossim/home/views.py
+++ ossim/home/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request,'home/home.html')
+
+
+def Login(request):
+    return render(request,'home/Login.html')
+
+def Desktop(request):
+    return render(request,'home/Desktop.html')
+
+def Task(request):
+    return render(request,'home/Task.html')
