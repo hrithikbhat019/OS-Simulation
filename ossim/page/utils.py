84838c7559cc12434e3bb8d79635cf19615a04df
16CO142 <shreyaspandith1210@gmail.com>
2018-04-10 11:50:37 +0530

    lfu added

--- ossim/page/utils.py
+++ ossim/page/utils.py
@@ -1,7 +1,7 @@
 
 
 def fifo(data):
-    print("reached here")
+
     cache_size = data["size"]
     requests = data["requests"]
     cache = [-1 for _ in range(cache_size)]
@@ -100,6 +100,89 @@ def lru(data):
 	output["hit-ratio"] = round(float(hit)/n,2);
 
 	return output
+
+
+
+
+
+
+def lfu(data):
+	cache_size = data["size"]
+	requests = data["requests"]
+	cache = [-1 for _ in range(cache_size)]
+
+
+	output = {}
+	output["states"] = [];
+
+	victim = 0
+	hit = 0
+	n = len(requests)
+	print(n)
+	frequency=[ -1 for _ in range(max(requests)+1
+								  )]
+	print(len(frequency))
+	indx = -1
+	last_used = [-1 for _ in range(cache_size)]
+	time = 0
+	fill = 0
+	v=[-1 for _ in range(cache_size)]
+
+	for request in requests:
+		frequency[request]=frequency[request]+1
+		print(frequency)
+		try:
+			indx = cache.index(request)
+		except ValueError:
+			indx = -1
+
+		if( indx != -1):
+			hit = hit + 1;
+
+			last_used[indx] = time
+			state = { "status": "H",
+					  "cache": " ".join(str(e) for e in cache) ,
+					  "lru": " ".join(str(e) for e in last_used) }
+			output["states"].append(state)
+
+
+		else:
+			if(-1 in cache):
+				victim = cache.index(-1)
+			else:
+				v1 = [9999 for _ in range(max(requests) + 1
+										)]
+				for ele in cache:
+					v1[ele]=frequency[ele]
+
+
+
+				minfreq = v1.index(min(v1))
+				victim=cache.index(minfreq)
+
+			cache[victim] = request
+			last_used[victim] = time
+
+			state = { "status": "M",
+					  "cache": " ".join(str(e) for e in cache) ,
+					  "lfu": " ".join(str(e) for e in last_used) }
+			output["states"].append(state)
+
+		time = time + 1
+
+	output["hits"] = hit
+	output["miss"] = n - hit
+	output["hit-ratio"] = round(float(hit)/n,2);
+
+	return output
+
+
+
+
+
+
+
+
 
 def opt(data):
 	cache_size = data["size"]
