127f965ef17fe3ba92024f699df75cf1ce8af0c2
Siddesh LC <siddeshlc8@gmail.com>
2018-04-11 19:24:42 +0530

    Deadlock App Updated

--- ossim/synchro/urls.py
+++ ossim/synchro/urls.py
@@ -5,7 +5,5 @@ from . import views
 app_name = 'synchro'
 urlpatterns =[
     url(r'^semaphores/$', views.semaphores, name='semaphores'),
-    url(r'^deadlocks/$', views.deadlocks, name='deadlocks'),
     url(r'^semaphores/demo/(?P<pk>[0-9]+)/$', views.sem_demo, name='sem_demo'),
-    url(r'^bankalgo/$', views.bankalgo, name='bankalgo'),
 ]
